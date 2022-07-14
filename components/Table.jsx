5bbc3d51c133e936c9c8eb61be98189767532312
Dave <daveg7lee@gmail.com>
2022-07-14 20:07:06 +0900

    table style

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,6 +1,6 @@
 const Table = ({ scores, deleteScore, Admin }) => {
   return (
-    <table className="w-full border-separate p-4">
+    <table className="w-full border-separate p-4 bg-bgColor dark:bg-slate-800 dark:text-white">
       <thead>
         <tr>
           <th>Score</th>
@@ -13,19 +13,19 @@ const Table = ({ scores, deleteScore, Admin }) => {
       </thead>
       {scores.map((score) => (
         <tr key={score.id} style={{ marginTop: "0.5rem" }}>
-          <td className="text-center">
+          <td className="text-center py-1">
             {score.type === "Demerit" ? score.score * -1 : score.score}
           </td>
-          <td className="text-center">{score.article}</td>
-          <td className="text-center">
+          <td className="text-center py-1">{score.article}</td>
+          <td className="text-center py-1">
             {score.date.replace(/T.*/, "").split("-").join("-")}
           </td>
-          <td className="text-center">{score.uploader}</td>
-          <td className="text-center">{score.detail}</td>
+          <td className="text-center py-1">{score.uploader}</td>
+          <td className="text-center py-1">{score.detail}</td>
           {Admin && (
-            <td className="text-center" onClick={deleteScore}>
+            <td className="text-center py-1" onClick={deleteScore}>
               <h1
-                className="cursor-pointer bg-blueColor py-2 px-1"
+                className="cursor-pointer bg-blueColor py-2 px-1 rounded"
                 id={score.id}
               >
                 Delete
