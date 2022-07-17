d869e9fc1292e805221301e83df8ac2502ada99c
Dave <daveg7lee@gmail.com>
2022-07-17 20:03:44 +0900

    bug fix

--- pages/searchUser.jsx
+++ pages/searchUser.jsx
@@ -107,7 +107,7 @@ const SearchUser = () => {
               <select
                 value={sortType}
                 onChange={(event) => setSortType(event.target.value)}
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
               >
                 <option value={"username"}>이름</option>
                 <option value={"scores"}>벌점</option>
