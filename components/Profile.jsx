a0b1ab36dd8593e6eb2d4a83d7f6e01cf9b0953b
daveg7lee <daveg7lee@gmail.com>
2022-09-25 18:56:52 +0900

    no console log

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,6 @@ import useUser from "../hooks/useUser";
 
 const Profile = () => {
   const { data, loading } = useUser();
-  console.log(data?.me)
   return (
     <div className="min-h-screen flex items-center justify-center pt-16">
       {loading ? <Loading /> : <ProfileBox me={data?.me} />}
