9e5e6c23db3e3953764d6775ab49a1fc3021abbc
daveg7lee <daveg7lee@gmail.com>
2023-05-30 19:50:56 +0900

    ✨ 벌점 및 상점 로직 업데이트

--- components/searchUser/SearchTable.jsx
+++ components/searchUser/SearchTable.jsx
@@ -1,7 +1,5 @@
 import {
   Button,
-  Grid,
-  GridItem,
   Modal,
   ModalBody,
   ModalContent,
@@ -42,19 +40,19 @@ const DELETE_SCORE_BY_USER = gql`
   }
 `;
 
-const SearchTable = ({ user, totalScore, totalMerit, refetch }) => {
+const SearchTable = ({ user, refetch }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [deleteScoreMutation] = useMutation(DELETE_SCORE);
   const [deleteScoreByUserMutation] = useMutation(DELETE_SCORE_BY_USER);
 
   const deleteScore = async (e) => {
     e.preventDefault();
-    if (!confirm("진짜 지울거야¿ 진짜로¿ 진짜¿ by 가은")) {
-      alert("좋은 선택이야!, by 가은");
+    if (!confirm("진짜 지울거야¿ 진짜로¿ 진짜¿")) {
+      alert("좋은 선택이야!");
       return;
     }
-    if (!confirm("후회 안 해¿ by 가은")) {
-      alert("좋은 선택이야!, by 가은");
+    if (!confirm("후회 안 해¿")) {
+      alert("좋은 선택이야!");
       return;
     }
 
@@ -77,17 +75,17 @@ const SearchTable = ({ user, totalScore, totalMerit, refetch }) => {
 
   const deleteScoreByUser = async (e) => {
     e.preventDefault();
-    if (!confirm("진짜 지울거야¿ 진짜로¿ 진짜¿ by 가은")) {
-      alert("좋은 선택이야!, by 가은");
+    if (!confirm("진짜 지울거야¿ 진짜로¿ 진짜¿")) {
+      alert("좋은 선택이야!");
       return;
     }
-    if (!confirm("후회 안 해¿ by 가은")) {
-      alert("좋은 선택이야!, by 가은");
+    if (!confirm("후회 안 해¿")) {
+      alert("좋은 선택이야!");
       return;
     }
     if (
       !confirm(
-        "벌점을 삭제한 후에는 되돌리기 굉장히 힘들 수 있습니다. 해당 사항을 인지하셨습니까?"
+        "벌점을 삭제후 7일 뒤에는 되돌릴 수 없습니다. 해당 사항을 인지하셨습니까?"
       )
     ) {
       alert("좋은 선택이야!");
@@ -113,48 +111,21 @@ const SearchTable = ({ user, totalScore, totalMerit, refetch }) => {
   };
 
   return (
-    <Grid key={user.id} templateColumns="repeat(6, 1fr)" py={5}>
-      <GridItem
-        w="full"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        {user.grade}
-      </GridItem>
-      <GridItem
-        w="full"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Avatar src={user.avatar} />
-      </GridItem>
-      <GridItem
-        w="full"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        {user.username}
-      </GridItem>
-      <GridItem
-        w="full"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        {totalScore}
-      </GridItem>
-      <GridItem
-        w="full"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        {totalMerit}
-      </GridItem>
-      <Button onClick={onOpen}>Detail</Button>
+    <>
+      <Tr key={user.id}>
+        <Td>{user.grade}</Td>
+        <Td>
+          <Avatar src={user.avatar} />
+        </Td>
+        <Td>{user.username}</Td>
+        <Td>{user.fullScores}</Td>
+        <Td>{user.fullMerit}</Td>
+        <Td>{user.totalScores}</Td>
+        <Td>{user.totalMerit}</Td>
+        <Td>
+          <Button onClick={onOpen}>Detail</Button>
+        </Td>
+      </Tr>
       <Modal
         isOpen={isOpen}
         onClose={onClose}
@@ -212,7 +183,7 @@ const SearchTable = ({ user, totalScore, totalMerit, refetch }) => {
           </ModalBody>
         </ModalContent>
       </Modal>
-    </Grid>
+    </>
   );
 };
 
--- pages/searchUser.jsx
+++ pages/searchUser.jsx
@@ -1,5 +1,17 @@
 import { gql, useQuery } from "@apollo/client";
-import { Box, Center, Heading, Select, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Heading,
+  Select,
+  Spinner,
+  TableContainer,
+  Tbody,
+  Th,
+  Thead,
+  Tr,
+  Table,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import AdminOnlyPage from "../components/ProtectedPages/AdminOnlyPage";
 import SearchTable from "../components/searchUser/SearchTable";
@@ -15,6 +27,8 @@ const SEE_USERS = gql`
         type
         totalScores
         totalMerit
+        fullScores
+        fullMerit
         scores {
           id
           score
@@ -41,19 +55,23 @@ const SearchUser = () => {
           ? [...data.seeUsers.users]
           : sortType === "demerit"
           ? [...data.seeUsers.users].sort(
-              (a, b) => a.totalScores - b.totalScores
+              (a, b) => b.totalScores - a.totalScores
             )
           : sortType === "merit"
           ? [...data.seeUsers.users].sort((a, b) => b.totalMerit - a.totalMerit)
+          : sortType === "totalDemerit"
+          ? [...data.seeUsers.users].sort((a, b) => b.fullScores - a.fullScores)
+          : sortType === "totalMerit"
+          ? [...data.seeUsers.users].sort((a, b) => b.fullMerit - a.fullMerit)
           : sortType === "grade" && [
-              ...data.seeUsers.users.filter((user) => user.grade === "G5"),
-              ...data.seeUsers.users.filter((user) => user.grade === "G6"),
-              ...data.seeUsers.users.filter((user) => user.grade === "G7"),
-              ...data.seeUsers.users.filter((user) => user.grade === "G8"),
-              ...data.seeUsers.users.filter((user) => user.grade === "G9"),
-              ...data.seeUsers.users.filter((user) => user.grade === "G10"),
-              ...data.seeUsers.users.filter((user) => user.grade === "G11"),
               ...data.seeUsers.users.filter((user) => user.grade === "G12"),
+              ...data.seeUsers.users.filter((user) => user.grade === "G11"),
+              ...data.seeUsers.users.filter((user) => user.grade === "G10"),
+              ...data.seeUsers.users.filter((user) => user.grade === "G9"),
+              ...data.seeUsers.users.filter((user) => user.grade === "G8"),
+              ...data.seeUsers.users.filter((user) => user.grade === "G7"),
+              ...data.seeUsers.users.filter((user) => user.grade === "G6"),
+              ...data.seeUsers.users.filter((user) => user.grade === "G5"),
             ];
       setUserData(users);
     }
@@ -77,19 +95,35 @@ const SearchUser = () => {
             >
               솔로몬 고위험자
             </Heading>
-            {userData.map((user) => {
-              if (user.totalScores <= -15) {
-                return (
-                  <SearchTable
-                    key={user.id}
-                    user={user}
-                    totalScore={user.totalScores}
-                    totalMerit={user.totalMerit}
-                    refetch={refetch}
-                  />
-                );
-              }
-            })}
+            <TableContainer>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>학년</Th>
+                    <Th>프로필</Th>
+                    <Th>이름</Th>
+                    <Th>올해 벌점</Th>
+                    <Th>올해 상점</Th>
+                    <Th>이번달 벌점</Th>
+                    <Th>이번달 상점</Th>
+                    <Th>전체보기</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {userData?.map((user) => {
+                    if (user.totalScores <= -15) {
+                      return (
+                        <SearchTable
+                          key={user.id}
+                          user={user}
+                          refetch={refetch}
+                        />
+                      );
+                    }
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
             <Box
               display="flex"
               alignItems="center"
@@ -101,25 +135,39 @@ const SearchUser = () => {
             >
               <Heading fontSize="2xl">전체 보기</Heading>
               <Select
-                w={20}
+                w="32"
                 value={sortType}
                 onChange={(event) => setSortType(event.target.value)}
               >
                 <option value={"username"}>이름</option>
-                <option value={"demerit"}>벌점</option>
-                <option value={"merit"}>상점</option>
+                <option value={"demerit"}>이번달 벌점</option>
+                <option value={"merit"}>이번달 상점</option>
+                <option value={"totalDemerit"}>올해 벌점</option>
+                <option value={"totalMerit"}>올해 상점</option>
                 <option value="grade">학년</option>
               </Select>
             </Box>
-            {userData.map((user) => (
-              <SearchTable
-                key={user.id}
-                user={user}
-                totalScore={user.totalScores}
-                totalMerit={user.totalMerit}
-                refetch={refetch}
-              />
-            ))}
+            <TableContainer>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>학년</Th>
+                    <Th>프로필</Th>
+                    <Th>이름</Th>
+                    <Th>올해 벌점</Th>
+                    <Th>올해 상점</Th>
+                    <Th>이번달 벌점</Th>
+                    <Th>이번달 상점</Th>
+                    <Th>전체보기</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {userData?.map((user) => (
+                    <SearchTable key={user.id} user={user} refetch={refetch} />
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
           </Box>
         )}
       </Box>
